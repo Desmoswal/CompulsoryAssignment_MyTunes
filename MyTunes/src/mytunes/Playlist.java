54f5562df7e807cfaf2ee02d52a06f05ab688221
kivulallo <kris88a1@easv365.dk>
2016-11-30 13:02:24 +0100

    started doing playlist framework

---
+++ MyTunes/src/mytunes/Playlist.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mytunes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Kristof
+ */
+public class Playlist {
+    private String name;
+    private ArrayList<String> songs;
+    private
+    
+}
---
+++ MyTunes/src/mytunes/Song.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mytunes;
+
+/**
+ *
+ * @author Kristof
+ */
+public class Song {
+    private String path;
+    private String artist;
+    private String title;
+    private String time;
+
+    public Song(String path) {
+        this.path = path;
+    }
+    
+    
+}
